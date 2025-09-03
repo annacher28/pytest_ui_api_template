9d479ba359a82a3b08c44905ddfec71f6bb4b1d1
annacher28 <cherepanovaann2006@gmail.com>
2025-09-03 17:03:38 +0300

    Описание изменений

--- pytest_ui_api_template/test/simple_test.py
+++ pytest_ui_api_template/test/simple_test.py
@@ -1,2 +1,14 @@
-def test_just_a():
-    assert 2==2
+from page.AuthPage import AuthPage
+from pytest_ui_api_template.test.conftest import browser
+
+def test_first(browser):
+    auth_page = AuthPage(browser)
+    auth_page.go()
+
+def test_auth():	
+    auth_page = AuthPage(browser)
+    auth_page.go()
+    auth_page.login_as("логин", "пароль")
+
+ # Проверяем, что после запуска теста URL заканчивается заданной подстрокой:
+    assert auth_page.get_current_url().endswith("...")
